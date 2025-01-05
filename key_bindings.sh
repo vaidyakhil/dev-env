0c84bbe2ae4d2e7d3dc7c0509c7afe816cb76f54
Akhil Vaidya <vaidyakhil@gmail.com>
2025-01-05 12:40:24 +0530

    docs: add comment explaning what to do to make word delete keybinding work

--- key_bindings.sh
+++ key_bindings.sh
@@ -1,10 +1,3 @@
+# for this to work
+# enable -> Use Option as Meta Key in Terminal settings -> Keyboard
 bindkey '^[[1;9D' backward-kill-word      # ⌥+Delete (delete word to the left)
-# bindkey '^[[1;10D' kill-word              # ⌥+Shift+Delete (delete word to the right)
-
-# Move to the start/end of the line
-bindkey '^[[1;5D' beginning-of-line  # Ctrl+←
-bindkey '^[[1;5C' end-of-line        # Ctrl+→
-
-# Delete all text to the left/right
-bindkey '^[[3;5~' backward-kill-line   # Ctrl+Delete
-bindkey '^[[3;6~' kill-line            # Ctrl+Shift+Delete
